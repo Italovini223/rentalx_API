860457a93ecb91d7c602101ebee4f673fb3b880c
Italovini223 <italovinicius2018@gmail.com>
2022-12-16 00:10:06 -0300

    ✨ feat: add all specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,28 @@
+import { ICreateSpecificationDTO, ISpecificationRepository } from "./ISpecificationRepository";
+import {Specification} from '../model/Specification'
+
+class SpecificationsRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+    
+  constructor() {
+    this.specifications = [];
+  }
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+    
+    Object.assign(specification, {
+      name,
+      description,
+      create_at: new Date()
+    });
+    
+    this.specifications.push(specification);
+  }
+  
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(spec => spec.name === name);
+    return specification
+  }
+}
+
+export { SpecificationsRepository }
